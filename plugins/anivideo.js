f56dbed993bd66475a5502c932b878c2adbe142c
MASK SER <94510246+mask-sir@users.noreply.github.com>
2024-06-05 20:51:06 +0530

    Create anivideo.js

---
+++ plugins/anivideo.js
@@ -0,0 +1,39 @@
+const { izumi,mode, getJson } = require("../lib/");
+const config = require("../config");
+izumi ({
+    pattern: "naruto",
+    fromMe: mode,
+    desc: "random Naruto anime videos",
+    type: "AnimeVideo",
+}, async (message, match) => {
+const { result } = await getJson('https://api.maskser.me/api/anime/naruto?apikey=izumi-v3')
+message.sendFromUrl(result.url,{caption: `${config.CAPTION`})
+});
+
+izumi ({
+    pattern: "anime",
+    fromMe: mode,
+    desc: "random  anime videos",
+    type: "AnimeVideo",
+}, async (message, match) => {
+const { result } = await getJson('https://api.maskser.me/api/anime/anivideo?apikey=izumi-v3')
+message.sendFromUrl(result.url,{caption: `${config.CAPTION`})
+});
+ izumi ({
+    pattern: "mstatus",
+    fromMe: mode,
+    desc: "random Malayalam status videos",
+    type: "media",
+}, async (message, match) => {
+const { result } = await getJson('https://api.maskser.me/api/randomvideo/msts?apikey=izumi-v3')
+message.sendFromUrl(result.video,{caption: `${config.CAPTION`})
+});
+ izumi ({
+    pattern: "sexy",
+    fromMe: mode,
+    desc: "random girls reels videos",
+    type: "media",
+}, async (message, match, client) => {
+let url = `https://api.maskser.me/api/randomvideo/sexy?apikey=izumi-v3`
+message.sendFromUrl(url,{caption: `${config.CAPTION`})
+});
