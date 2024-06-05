813d196b96ff24bce1271ed110a8eb50ac275eae
Eypz <105191630+sataniceypz@users.noreply.github.com>
2024-06-05 20:32:17 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,43 @@
+const { Sequelize } = require("sequelize");
+const fs = require("fs");
+require('dotenv').config();
+
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env', override: true });
+
+// Function to convert text to boolean
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+// Function to convert string to boolean
+const toBool = (x) => (x && x.toLowerCase() === 'true') || false;
+
+// Define the Sequelize instance based on DATABASE_URL
+const DATABASE_URL = process.env.DATABASE_URL === undefined ? './database.db' : process.env.DATABASE_URL;
+DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
+// Export configuration variables
+module.exports = {
+  HANDLERS: (process.env.PREFIX || '^[.,!]').trim(),
+  BRANCH: "main",
+  MODE: (process.env.MODE || 'private').toLowerCase(),
+  ERROR_MSG: toBool(process.env.ERROR_MSG) || true,
+  LOG_MSG: toBool(process.env.LOG_MSG) || true,
+  READ_CMD: toBool(process.env.READ_CMD),
+  SESSION_ID: process.env.SESSION_ID || null,
+  MENU_URL: process.env.MENU_URL || "https://i.imgur.com/8H2hM6D.jpeg",
+  READ_MSG: toBool(process.env.READ_MSG),
+  OWNER_NAME: process.env.OWNER_NAME || "Eypz God",
+  BOT_NAME: process.env.BOT_NAME || "Izumi-v3",
+  SUDO: process.env.SUDO || "917994489493,919544951258",
+  PACKNAME: process.env.PACKNAME || "Izumi-v3",
+  PROCESSNAME: process.env.PROCESSNAME || "Izumi-v3",
+  AUTHOR: process.env.AUTHOR || "Eypz God",
+  DELETED_LOG_CHAT: process.env.DELETED_LOG_CHAT || false,
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
+  HEROKU: toBool(process.env.HEROKU) || false,
+  DATABASE_URL: DATABASE_URL,
+  DATABASE:
+       DATABASE_URL === './database.db' ? new Sequelize({dialect: 'sqlite', storage: DATABASE_URL, logging: false,}) : new Sequelize(DATABASE_URL, {dialect: 'postgres', ssl: true, protocol: 'postgres', dialectOptions: {native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false,}),
+  DEBUG: DEBUG
+};
