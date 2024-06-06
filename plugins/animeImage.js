c86f1b279dd249a21e93c20bcac54fca81634ff6
mask ser <444shijil@mail.com>
2024-06-06 08:54:34 +0530

    update bot name in menu

---
+++ plugins/animeImage.js
@@ -0,0 +1,40 @@
+const { izumi, mode, getJson } = require("../lib");
+const config = require("../config");
+izumi({
+  pattern: "waifu",
+  fromMe: mode,
+  desc: "Random anime images",
+  type: "AnimeImage",
+}, async (message, match) => {
+  var { url } = await getJson('https://api.waifu.pics/sfw/waifu');
+  await message.sendFromUrl(url,{caption: `${config.CAPTION}`});
+});
+
+izumi({
+  pattern: "neko",
+  fromMe: mode,
+  desc: "Random anime images",
+  type: "AnimeImage",
+}, async (message, match) => {
+  var { url } = await getJson('https://api.waifu.pics/sfw/neko');
+  await message.sendFromUrl(url,{caption: `${config.CAPTION}`});
+});
+izumi({
+  pattern: "loli",
+  fromMe: mode,
+  desc: "Random anime images",
+  type: "AnimeImage",
+}, async (message, match) => {
+  var { url } = await getJson('https://api.waifu.pics/sfw/neko');
+  await message.sendFromUrl(url,{caption: `${config.CAPTION}`});
+});
+
+
+izumi({
+  pattern: "shinobu",
+  fromMe: true,
+  desc: "Random anime images",
+  type: "AnimeImage",
+}, async (message, match) => {
+  await message.sendFromUrl('https://api.maskser.me/api/anime/loli?apikey=izumi-v3',{caption: `${config.CAPTION}`});
+});
