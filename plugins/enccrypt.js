adcc9bec7a8145728515d296e903b8c4cb58c837
mask ser <444shijil@mail.com>
2024-06-08 14:10:46 +0530

    new command obfuscate

--- plugins/enccrypt.js
+++ plugins/enccrypt.js
@@ -18,3 +18,74 @@ const {
  		message.reply(`${result.error}`)
  	};
  });
+izumi(	{pattern: 'obfuscate ?(.*)',		fromMe: mode,	desc: 'javascript obfuscator',		type: 'generator' 	},async (message, match) => {
+const data = {}
+
+data.value = {
+    compact: true,
+    config: '',
+    controlFlowFlattening: false,
+    controlFlowFlatteningThreshold: 0.75,
+    deadCodeInjection: false,
+    deadCodeInjectionThreshold: 0.4,
+    debugProtection: false,
+    debugProtectionInterval: 0,
+    disableConsoleOutput: false,
+    domainLock: [],
+    domainLockRedirectUrl: 'about:blank',
+    exclude: [],
+    forceTransformStrings: [],
+    identifierNamesCache: null,
+    identifierNamesGenerator: 'hexadecimal',
+    identifiersPrefix: '',
+    identifiersDictionary: [],
+    ignoreImports: false,
+    inputFileName: '',
+    log: true,
+    numbersToExpressions: false,
+    optionsPreset: 'default',
+    renameGlobals: false,
+    renameProperties: false,
+    renamePropertiesMode: 'safe',
+    reservedNames: [],
+    reservedStrings: [],
+    stringArrayRotate: true,
+    seed: 707066159,
+    selfDefending: false,
+    stringArrayShuffle: true,
+    simplify: true,
+    sourceMap: false,
+    sourceMapBaseUrl: '',
+    sourceMapFileName: '',
+    sourceMapMode: 'separate',
+    sourceMapSourcesMode: 'sources-content',
+    splitStrings: false,
+    splitStringsChunkLength: 10,
+    stringArray: true,
+    stringArrayCallsTransform: false,
+    stringArrayCallsTransformThreshold: 0.5,
+    stringArrayEncoding: [ 'none' ],
+    stringArrayIndexesType: [ 'hexadecimal-number' ],
+    stringArrayIndexShift: true,
+    stringArrayWrappersChainedCalls: true,
+    stringArrayWrappersCount: 1,
+    stringArrayWrappersParametersMaxCount: 2,
+    stringArrayWrappersType: 'variable',
+    stringArrayThreshold: 0.75,
+    target: 'browser',
+    transformObjectKeys: false,
+    unicodeEscapeSequence: false
+  }
+    
+data.code = message.reply_message.text
+const axios = require("axios");
+var clientServerOptions = {
+url: 'https://api.maskser.me/api/javascript-obfuscator',
+data: JSON.stringify(data),
+method: 'POST',
+headers: {'Content-Type': 'application/json'}
+}
+
+const res = await axios.request(clientServerOptions)
+await message.send(res.data.code)
+});
