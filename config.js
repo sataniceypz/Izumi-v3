bfeebde55af9f8d57ad0ed93db02a520c4237b0d
Eypz <105191630+sataniceypz@users.noreply.github.com>
2024-06-13 11:27:19 +0530

    Config update

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ module.exports = {
   LOG_MSG: toBool(process.env.LOG_MSG) || true,
   READ_CMD: toBool(process.env.READ_CMD),
   SESSION_ID: process.env.SESSION_ID || null,
-  MENU_URL: process.env.MENU_URL || "https://telegra.ph/file/6969611f421f0855a5401.jpg",
+  MENU_URL: process.env.MENU_URL || "https://i.imgur.com/cO0TZJv.jpeg",
   CAPTION: process.env.CAPTION || "Iᴢᴜᴍɪ",
   READ_MSG: toBool(process.env.READ_MSG),
   OWNER_NAME: process.env.OWNER_NAME || "Eypz God",
