b9639f20ceed253301680998df8c8dfbc1ad77a4
mask ser <appushijil72@gmail.com>
2024-06-18 21:40:24 +0530

    new command: demonslayer

--- plugins/anivideo.js
+++ plugins/anivideo.js
@@ -85,3 +85,24 @@ const url = apiUrl + 'api/anime/onepiece?apikey=izumi-v3'
     caption: `${config.CAPTION}`
   });
 });
+izumi ({
+    pattern: "demonslayer",
+    fromMe: mode,
+    desc: "random demonslayer anime status ",
+    type: "AnimeVideo",
+}, async (message, match) => {
+const url = apiUrl + 'api/anime/demonslayer?apikey=izumi-v3'
+  message.sendFromUrl(url, {
+    contextInfo: {
+      mentionedJid: [message.sender],
+      forwardingScore: 1,
+      isForwarded: true,
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: '120363298577467093@newsletter',
+        newsletterName: "Iᴢᴜᴍɪ-ᴠ3",
+        serverMessageId: -1
+      }
+    },
+    caption: `${config.CAPTION}`
+  });
+});
