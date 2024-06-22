5e8a12d56a444acbe8ee3df22717ba79a1041ee1
mask ser <appushijil72@gmail.com>
2024-06-22 20:17:34 +0530

    new command: ss,fullss

---
+++ plugins/ss.js
@@ -0,0 +1,30 @@
+const { izumi, mode, getUrl } = require("../lib/");
+const config = require("../config");
+izumi(
+    {
+        pattern: "fullss ?(.*)",
+        fromMe: mode,
+        desc: "capture website screenshot",
+        type: "download",
+    },
+    async (message, match, m) => {
+match = match || message.reply_message.text;
+	if(!match) return await message.reply(`*_Need a link_*\n*eg:- .fullss https://github.com/sataniceypz/Izumi-v3*`);
+	let url = await getUrl(match);
+	let res = apiUrl + `api/screenshot?url=${url}&type=phone&full=true&apikey=maskser`
+await message.sendFromUrl(res)
+});
+izumi(
+    {
+        pattern: "ss ?(.*)",
+        fromMe: mode,
+        desc: "capture website screenshot",
+        type: "downloader",
+    },
+    async (message, match, m) => {
+match = match || message.reply_message.text;
+	if(!match) return await message.reply(`*_Need a link_*\n*eg:- .ss https://github.com/sataniceypz/Izumi-v3*`);
+	let url = await getUrl(match)
+	let res = apiUrl + `api/screenshot?url=${url}&type=desktop&full&apikey=maskser`
+await message.sendFromUrl(res)
+});
