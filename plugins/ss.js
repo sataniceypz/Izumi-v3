6220815db8366217bb6aa8bc219458dc259aa8cd
mask ser <appushijil72@gmail.com>
2024-06-22 20:34:49 +0530

    fix

--- plugins/ss.js
+++ plugins/ss.js
@@ -1,5 +1,6 @@
 const { izumi, mode, getUrl } = require("../lib/");
 const config = require("../config");
+
 izumi(
     {
         pattern: "fullss ?(.*)",
@@ -8,12 +9,18 @@ izumi(
         type: "download",
     },
     async (message, match, m) => {
-match = match || message.reply_message.text;
-	if(!match) return await message.reply(`*_Need a link_*\n*eg:- .fullss https://github.com/sataniceypz/Izumi-v3*`);
-	let url = await getUrl(match);
-	let res = apiUrl + `api/screenshot?url=${url}&type=phone&full=true&apikey=maskser`
-await message.sendFromUrl(res)
-});
+        try {
+            match = match || message.reply_message.text;
+            if (!match) return await message.reply(`*_Need a link_*\n*eg:- .fullss https://github.com/sataniceypz/Izumi-v3*`);
+            let url = await getUrl(match);
+            let res = apiUrl + `api/screenshot?url=${url}&type=phone&full=true&apikey=maskser`;
+            await message.sendFromUrl(res);
+        } catch (error) {
+            await message.reply('Failed to capture screenshot.');
+        }
+    }
+);
+
 izumi(
     {
         pattern: "ss ?(.*)",
@@ -22,9 +29,14 @@ izumi(
         type: "downloader",
     },
     async (message, match, m) => {
-match = match || message.reply_message.text;
-	if(!match) return await message.reply(`*_Need a link_*\n*eg:- .ss https://github.com/sataniceypz/Izumi-v3*`);
-	let url = await getUrl(match)
-	let res = apiUrl + `api/screenshot?url=${url}&type=desktop&full&apikey=maskser`
-await message.sendFromUrl(res)
-});
+        try {
+            match = match || message.reply_message.text;
+            if (!match) return await message.reply(`*_Need a link_*\n*eg:- .ss https://github.com/sataniceypz/Izumi-v3*`);
+            let url = await getUrl(match);
+            let res = apiUrl + `api/screenshot?url=${url}&type=desktop&full&apikey=maskser`;
+            await message.sendFromUrl(res);
+        } catch (error) {
+            await message.reply('Failed to capture screenshot.');
+        }
+    }
+);
