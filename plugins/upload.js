5651dea0997a1686082d4f765ea41a8050433066
mask ser <appushijil72@gmail.com>
2024-06-22 10:03:46 +0530

    fix upload

--- plugins/upload.js
+++ plugins/upload.js
@@ -1,34 +1,18 @@
-const { izumi, mode, parsedUrl } = require("../lib/");
+const { izumi, mode, parsedUrl} = require("../lib/");
 
 izumi(
   {
     pattern: "upload ?(.*)",
     fromMe: mode,
-    desc: "send file from any url",
+    desc: "send files from multiple URLs",
     type: "downloader",
   },
   async (message, match) => {
-    // Use match or fallback to the quoted text
     match = match || message.quoted?.text;
-    
-    // If neither match nor quoted text is available, send a prompt
     if (!match) {
       return await message.send("_reply to a url_");
     }
-
-    // Parse the URL
-    let url;
-    try {
-      url = await parsedUrl(match);
-    } catch (error) {
-      return await message.send("_Invalid URL provided_");
-    }
-
-    // Send the content from the URL
-    try {
-      await message.sendFromUrl(url);
-    } catch (error) {
-      await message.send("_Failed to send content from the URL_");
-    }
+        await message.sendFromUrl(parsedUrl(match));
+      
   }
 );
