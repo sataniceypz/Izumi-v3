e994b43be0b9943514816cc66d25378483b892dd
mask ser <appushijil72@gmail.com>
2024-06-23 11:49:35 +0530

    new command: help

--- plugins/list.js
+++ plugins/list.js
@@ -3,6 +3,7 @@ const {
 	commands,
 	mode
 } = require("../lib/");
+const config = require("../config");
 izumi({
 	pattern: 'list$',
 	fromMe: mode,
@@ -17,3 +18,79 @@ izumi({
 	})
 	await message.reply(msg.trim())
 })
+izumi({
+    pattern: 'help$',
+    fromMe: mode,
+    dontAddCommandList: true
+}, async (message, match) => {
+    try {
+        const now = new Date();
+        const formattedDate = now.toLocaleDateString();
+        const formattedTime = now.toLocaleTimeString();
+
+        let msg = `\n╭━━━〔 ${config.BOT_NAME} 〕━━━┈
+╭──────────────
+❖ │  *OWNER*: ${config.OWNER_NAME}
+❖ │  *MODE*: ${config.MODE}
+❖ │  *DATE*: ${formattedDate}
+❖ │  *TIME*: ${formattedTime}
+╰──────────────
+╰━━━━━━━━━━━━━━━┈\n\n`;
+
+        commands.forEach(command => {
+            if (!command.dontAddCommandList && command.pattern) {
+                const patternMatch = String(command.pattern).match(/(\W*)([A-Za-z0-9_ğüşiö ç]*)/);
+                if (patternMatch) {
+                    msg += `❖ ${patternMatch[2].trim()}\n${command.desc}\n\n`;
+                }
+            }
+        });
+
+        let eypz = {
+            key: {
+                participant: "0@s.whatsapp.net",
+                remoteJid: "120363280001854361@g.us"
+            },
+            message: {
+                productMessage: {
+                    product: {
+                        productImage: {
+                            mimetype: "image/jpeg",
+                            jpegThumbnail: Buffer.alloc(0)
+                        },
+                        title: "Command List",
+                        description: "List of available commands",
+                        currencyCode: "USD",
+                        priceAmount1000: "100000000000000",
+                        retailerId: "Eypz",
+                        productImageCount: 1
+                    },
+                    businessOwnerJid: "917994489493@s.whatsapp.net"
+                }
+            }
+        };
+
+        return await message.sendFromUrl(config.MENU_URL, {
+            fileLength: "5555544444",
+            gifPlayback: true,
+            contextInfo: {
+                externalAdReply: {
+                    title: config.BOT_NAME,
+                    body: config.OWNER_NAME,
+                    sourceUrl: "https://github.com/sataniceypz/Izumi-v3",
+                    mediaUrl: "https://instagram.com",
+                    mediaType: 1,
+                    showAdAttribution: true,
+                    renderLargerThumbnail: false,
+                    thumbnailUrl: "https://izumi-web.vercel.app/image/bot.png"
+                }
+            },
+            caption: msg
+        }, {
+            quoted: eypz
+        });
+
+    } catch (error) {
+        console.error('Error occurred:', error);
+    }
+});
