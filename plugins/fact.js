6492187c2537427a910d9072d44afe646e15c1db
Eypz <105191630+sataniceypz@users.noreply.github.com>
2024-07-12 02:30:42 +0530

    Create fact.js

---
+++ plugins/fact.js
@@ -0,0 +1,37 @@
+const { izumi, getJson } = require("../lib/");
+const config = require("../config");
+izumi(
+    {
+        pattern: "cat-fact",
+        fromMe: true,
+        desc: "facts about cat",
+        type: "fact",
+    },
+    async (message) => {
+        try {
+            let response = await getJson(`https://api-eypz.onrender.com/cat-fact`);
+            
+            let fact = response.fact;
+            
+            const eypz = {
+                text: fact,
+                contextInfo: {
+                    externalAdReply: {
+                        title: "Cat Fact",
+                        body: config.BOT_NAME,
+                        sourceUrl: "https://github.com/sataniceypz/Izumi-v3",
+                        mediaUrl: "https://github.com/sataniceypz/Izumi-v3",
+                        mediaType: 1,
+                        showAdAttribution: true,
+                        renderLargerThumbnail: true,
+                        thumbnailUrl: "https://api-eypz.onrender.com/cat"
+                    }
+                }
+            };
+            
+            await message.client.sendMessage(message.jid, eypz);
+        } catch (error) {
+            console.error("Error fetching cat fact:", error);
+        }
+    }
+);
