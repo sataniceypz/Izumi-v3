8109c6c15450a4debf9a43145af1cab74a205303
Eypz <105191630+sataniceypz@users.noreply.github.com>
2024-07-15 01:26:18 +0530

    api

--- config.js
+++ config.js
@@ -12,6 +12,7 @@ function convertToBool(text, fault = 'true') {
 // Function to convert string to boolean
 const toBool = (x) => (x && x.toLowerCase() === 'true') || false;
 global.apiUrl = 'https://api.maskser.me/'
+global.eypzApi = 'https://eypzgod-api.onrender.com/'
 
 // Define the Sequelize instance based on DATABASE_URL
 const DATABASE_URL = process.env.DATABASE_URL === undefined ? './database.db' : process.env.DATABASE_URL;
