cd090e72264a34a4d1ab3cf7bc8777579199e0cc
mask ser <appushijil72@gmail.com>
2024-07-28 15:40:38 +0530

    now url supports koyeb

--- plugins/imgur.js
+++ plugins/imgur.js
@@ -1,41 +1,43 @@
- const {
-	izumi,
-	Imgur,
-	mode
-} = require("../lib/");
-const ffmpeg = require('fluent-ffmpeg');
+const { izumi, mode,uploadToImageKit } = require("../lib/");
 izumi({
-	pattern: 'url ?(.*)',
-	fromMe: mode,
-	desc: 'upload files to imgur.com',
-	type: 'generator'
-}, async (m, text, client) => {
-	if(!m.quoted) return m.reply("reply to a image/audio/video message")
-if(m.quoted.image){
-const media = await m.quoted.download()
-		const res = await Imgur(media)
-		await m.reply(res.link)
-	} else if(m.quoted.video){
+    pattern: 'url ?(.*)',
+    fromMe: mode,
+    desc: 'upload files to imagekit.io',
+    type: 'generator'
+}, async (m, text) => {
+    if (!m.quoted) return m.reply("Reply to an image/audio/video message");
 
-		const media = await m.reply_message.download()
-		const res = await Imgur(media)
-		await m.reply(res.link)
-	} else if(m.quoted.audio){
-
-		try {
-			const media = await m.reply_message.download()
-			ffmpeg(media)
-				.outputOptions(["-y", "-filter_complex", "[0:a]showvolume=f=1:b=4:w=720:h=68,format=yuv420p[vid]", "-map", "[vid]", "-map 0:a"])
-				.save('output.mp4')
-				.on('end', async () => {
-					var res = await Imgur('output.mp4');
-					await m.reply(res.link)
-				})
-		} catch (e) {
-			await m.reply(e.message)
-		}
-	} else {
-		return m.reply("_Reply to a video/image/audio!_")
-	}
-
-})
+    try {
+        let media;
+        if (m.quoted.image) {
+            media = await m.quoted.download("buffer");
+            const uploadResponse = await uploadToImageKit(media, 'image');
+            if (uploadResponse && uploadResponse.url) {
+                await m.reply(uploadResponse.url);
+            } else {
+                await m.reply("Failed to upload image.");
+            }
+        } else if (m.quoted.video) {
+            media = await m.quoted.download("buffer");
+            const uploadResponse = await uploadToImageKit(media, 'video');
+            if (uploadResponse && uploadResponse.url) {
+                await m.reply(uploadResponse.url);
+            } else {
+                await m.reply("Failed to upload video.");
+            }
+        } else if (m.quoted.audio) {
+            media = await m.quoted.download("buffer");
+            const uploadResponse = await uploadToImageKit(media, 'audio');
+            if (uploadResponse && uploadResponse.url) {
+                await m.reply(uploadResponse.url);
+            } else {
+                await m.reply("Failed to upload audio.");
+            }
+        } else {
+            return m.reply("_Reply to a video/image/audio!_");
+        }
+    } catch (error) {
+        console.log('Error:', error);
+        await m.reply(`Error: ${error.message}`);
+    }
+});
