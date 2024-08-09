b50a1a332e9ae53e8167a96e02958cbd952386dd
Eypz <105191630+sataniceypz@users.noreply.github.com>
2024-08-09 19:38:23 +0530

    Pinterest Downloader

---
+++ plugins/pin.js
@@ -0,0 +1,37 @@
+const { izumi, mode, isUrl, getJson, parsedUrl } = require("../lib");
+const config = require("../config");
+izumi(
+  {
+    pattern: "pin ?(.*)",
+    fromMe: mode,
+    desc: "Search YouTube videos",
+    type: "downloader",
+  },
+  async (message, match) => {
+    try {
+      match = match || message.reply_message.text;
+
+      if (!match) {
+        await message.reply("Please provide a URL");
+        return;
+      }
+
+      const result = await getJson(eypzApi + `pin?url=${match}`);
+      if (!result || !result.media_urls) {
+        await message.reply("Invalid response from the API");
+        return;
+      }
+      const urlMp4 = Object.values(result.media_urls).find(url => url.endsWith('.mp4'));
+      
+      if (!urlMp4) {
+        await message.reply("No  media found");
+        return;
+      }
+      await message.sendFromUrl(urlMp4);
+
+    } catch (error) {
+      console.error(error);
+      await message.reply("An error occurred while processing your request.");
+    }
+  }
+);
